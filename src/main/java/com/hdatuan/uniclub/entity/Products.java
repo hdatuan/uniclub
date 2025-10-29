9402cab1d44f1b415fd5a80d06c4ff94f6106497
Hoang Tuan <hdatuaans496@gmail.com>
2025-10-30 02:13:26 +0700

    update Products entity

--- src/main/java/com/hdatuan/uniclub/entity/Products.java
+++ src/main/java/com/hdatuan/uniclub/entity/Products.java
@@ -1,6 +1,8 @@
 package com.hdatuan.uniclub.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,6 +15,7 @@ import java.time.LocalDateTime;
 public class Products {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String description;
